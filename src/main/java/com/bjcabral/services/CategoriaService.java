6b176f06f1196329d9ff5825548ccfbdca397db0
Bruno Cabral <bjcabral@outlook.com>
2020-04-28 22:54:47 +0100

    Criando repository e service para categoria

---
+++ src/main/java/com/bjcabral/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.bjcabral.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bjcabral.domain.Categoria;
+import com.bjcabral.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	 
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}

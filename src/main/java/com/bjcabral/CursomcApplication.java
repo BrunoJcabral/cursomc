b697a430b1f46fd7a6096048d2de6e2f82e5fc83
Bruno Cabral <bjcabral@outlook.com>
2020-04-28 23:39:45 +0100

    Criando operação de instanciação

--- src/main/java/com/bjcabral/CursomcApplication.java
+++ src/main/java/com/bjcabral/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.bjcabral;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.bjcabral.domain.Categoria;
+import com.bjcabral.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria categoria1 = new Categoria(null, "informatica"); 
+		Categoria categoria2 = new Categoria(null, "escritorio"); 
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+		
 	}
 
 }

04f1a16c3665be44cb2d76fd0a0b63d691785c0a
Bruno Cabral <bjcabral@outlook.com>
2020-05-03 10:44:56 +0100

    Cliente, tipocliente, telefones e endereços

--- src/main/java/com/bjcabral/CursomcApplication.java
+++ src/main/java/com/bjcabral/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bjcabral.domain.Categoria;
 import com.bjcabral.domain.Cidade;
+import com.bjcabral.domain.Cliente;
+import com.bjcabral.domain.Endereco;
 import com.bjcabral.domain.Estado;
 import com.bjcabral.domain.Produto;
+import com.bjcabral.domain.enums.TipoCliente;
 import com.bjcabral.repositories.CategoriaRepository;
 import com.bjcabral.repositories.CidadeRepository;
+import com.bjcabral.repositories.ClienteRepository;
+import com.bjcabral.repositories.EnderecoRepository;
 import com.bjcabral.repositories.EstadoRepository;
 import com.bjcabral.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -51,8 +60,6 @@ public class CursomcApplication implements CommandLineRunner{
 				
 		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
-		
 
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -66,6 +73,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2,cidade3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "Maria@gmail.com", "91234567890", TipoCliente.PESSOA_FISICA);
+		cli1.getTelefone().addAll(Arrays.asList("34271667", "31310023"));
+		
+		Endereco end1 = new Endereco(null, "Rua vasco cirquine", 25, "casa", "Sitio novo", cidade1, cli1);
+		Endereco end2 = new Endereco(null, "Rua Joao de Barros", 13, "casa", "Sitio novo", cidade2, cli1);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
+		
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(Arrays.asList(end1,end1));
+		
+		
 	}
 
 }

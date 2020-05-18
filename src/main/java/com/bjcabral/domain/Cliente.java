8acbb91855e3215d2b5a0865c0a95bb72b66a669
Bruno Cabral <bjcabral@outlook.com>
2020-05-18 20:08:47 +0100

    email não repetido na inserção do cliente

--- src/main/java/com/bjcabral/domain/Cliente.java
+++ src/main/java/com/bjcabral/domain/Cliente.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import com.bjcabral.domain.enums.TipoCliente;
@@ -26,6 +27,7 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	@JoinColumn(unique = true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
--- src/main/java/com/bjcabral/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bjcabral/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bjcabral.domain.Cliente;
 import com.bjcabral.domain.enums.TipoCliente;
 import com.bjcabral.dto.ClienteNewDTO;
+import com.bjcabral.repositories.ClienteRepository;
 import com.bjcabral.resources.exception.FieldMessage;
 import com.bjcabral.services.validation.Util.UtilBR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !UtilBR.isValidCNPJ(objDto.getCpfOuCnpj())) {			
 				list.add(new FieldMessage("cpfOuCnpj","Numero informado não corresponde ao CNPJ"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(cliente!=null) {
+			list.add(new FieldMessage("email","Email informado já foi cadastrado"));
 		}
 		
 		for (FieldMessage e : list) {
